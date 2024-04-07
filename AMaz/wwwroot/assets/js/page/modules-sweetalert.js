91bac78a913018aa6e9f7da4a91d4fe85cdaeefe
1Ir-is <mhuynk1005@gmail.com>
2024-04-07 18:16:46 +0700

    feat: change the selected of faculty

--- AMaz/wwwroot/assets/js/page/modules-sweetalert.js
+++ AMaz/wwwroot/assets/js/page/modules-sweetalert.js
@@ -1,5 +1,9 @@
 "use strict";
 
+$("#swal-2").click(function () {
+    // Trigger the form submission when the button is clicked
+    $(".need-validation").submit();
+});
 
 $(".need-validation").submit(function (event) {
     event.preventDefault();
@@ -36,7 +40,7 @@ $(".need-validation").submit(function (event) {
         isEmailExist(email).then(function (exists) {
             // Hide all email-related validation messages
             $('.email-validation-feedback').hide();
-            
+
             if (exists) {
                 // Email already exists, show validation message
                 $('#emailExistsValidationMessage').show();
@@ -71,8 +75,41 @@ $(".need-validation").submit(function (event) {
     }
 });
 
+$("#swal-2").click(function () {
+    // Check if any radio button is selected
+    if ($('input[name="value"]:checked').length === 0) {
+        // If no radio button is selected, show the validation message
+        $('#roleValidationMessage').show();
+    } else {
+        // If a radio button is selected, check if the form is valid
+        if ($('.wizard-content')[0].checkValidity()) {
+            // Check password validity
+            var password = $('input[name="password"]').val();
+            if (!isValidPassword(password)) {
+                // Password does not meet requirements, show validation message
+                $('#passwordValidationMessage').show();
+                return;
+            } else {
+                // Hide password validation message if password is valid
+                $('#passwordValidationMessage').hide();
+            }
 
-
+            // Display sweet alert if the form is valid
+            $('#roleValidationMessage').hide();
+            swal('Account Created', 'Your account has been created successfully!', 'success')
+                .then((value) => {
+                    if (value) {
+                        // If the user clicks OK on the alert, redirect to admin.html
+                        var redirectUrl = "/Account"
+                        window.location.href = redirectUrl;
+                    }
+                });
+        } else {
+            // If the form is not valid, trigger form validation
+            $('.wizard-content').addClass('was-validated');
+        }
+    }
+});
 
 // Function to validate the password format
 function isValidPassword(password) {
@@ -136,71 +173,71 @@ $("#swal-4").click(function () {
     $(".needs-validation-profile").submit();
 });
 
-$(".needs-validation-profile").submit(function(event) {
-  event.preventDefault();
-  event.stopPropagation();
+$(".needs-validation-profile").submit(function (event) {
+    event.preventDefault();
+    event.stopPropagation();
 
-  // Validate the form
-  if (this.checkValidity() === false) {
-    // Add the was-validated class to trigger Bootstrap validation styles
-    $(this).addClass("was-validated");
-  } else {
-    // If the form is valid, show success message
-    swal({
-      title: 'Changes Saved',
-      text: 'Your profile information has been updated successfully!',
-      icon: 'success',
-    })
-    .then((value) => {
-        if (value) {
-            // If the user clicks OK on the alert, redirect to admin.html
-            window.location.href = '../UserUI/user-profile.html';
-        }
-    });
-  }
+    // Validate the form
+    if (this.checkValidity() === false) {
+        // Add the was-validated class to trigger Bootstrap validation styles
+        $(this).addClass("was-validated");
+    } else {
+        // If the form is valid, show success message
+        swal({
+            title: 'Changes Saved',
+            text: 'Your profile information has been updated successfully!',
+            icon: 'success',
+        })
+            .then((value) => {
+                if (value) {
+                    // If the user clicks OK on the alert, redirect to admin.html
+                    window.location.href = '../UserUI/user-profile.html';
+                }
+            });
+    }
 });
 
 $("#swal-6").click(function (event) {
-  // Prevent the default form submission
-  event.preventDefault();
+    // Prevent the default form submission
+    event.preventDefault();
 
-  // Get the values of password and confirm password fields
-  var password = $('input[name="password"]').val();
-  var confirmPassword = $('input[name="confirmPassword"]').val();
+    // Get the values of password and confirm password fields
+    var password = $('input[name="password"]').val();
+    var confirmPassword = $('input[name="confirmPassword"]').val();
 
-  // Check if the passwords match
-  if (password !== confirmPassword) {
-      // Passwords don't match, show an alert
-      swal('Error', 'Passwords do not match. Please make sure your passwords match.', 'error');
-      return; // Stop further processing
-  }
+    // Check if the passwords match
+    if (password !== confirmPassword) {
+        // Passwords don't match, show an alert
+        swal('Error', 'Passwords do not match. Please make sure your passwords match.', 'error');
+        return; // Stop further processing
+    }
 
-  // Check if the form is valid
-  if ($('.wizard-content')[0].checkValidity()) {
-      // Display sweet alert if the form is valid
-      swal({
-        title: 'Are you sure?',
-        text: 'Once Reseted, This account will log in with the new password!',
-        icon: 'warning',
-        buttons: true,
-        dangerMode: true,
-    }).then((willReset) => {
-        if (willReset) {
-            swal('Reset Password Successfully!', {
-                icon: 'success',
-            }).then(() => {
-                // If the user clicks OK on the alert, redirect to admin.html
-                var redirectUrl = "/Account";
-                window.location.href = redirectUrl;
-            });
-        } else {
-            swal('The password remains unchanged.');
-        }
-      });
-  } else {
-      // If the form is not valid, trigger form validation
-      $('.wizard-content').addClass('was-validated');
-  }
+    // Check if the form is valid
+    if ($('.wizard-content')[0].checkValidity()) {
+        // Display sweet alert if the form is valid
+        swal({
+            title: 'Are you sure?',
+            text: 'Once Reseted, This account will log in with the new password!',
+            icon: 'warning',
+            buttons: true,
+            dangerMode: true,
+        }).then((willReset) => {
+            if (willReset) {
+                swal('Reset Password Successfully!', {
+                    icon: 'success',
+                }).then(() => {
+                    // If the user clicks OK on the alert, redirect to admin.html
+                    var redirectUrl = "/Account";
+                    window.location.href = redirectUrl;
+                });
+            } else {
+                swal('The password remains unchanged.');
+            }
+        });
+    } else {
+        // If the form is not valid, trigger form validation
+        $('.wizard-content').addClass('was-validated');
+    }
 });
 
 
@@ -224,20 +261,19 @@ $("#swal-5").submit(function (event) {
 });
 
 
-$(document).ready(function() {
-  $("#homeworkForm").submit(function(event) {
-      // Check if the checkbox is checked
-      if (!$("#agree").prop("checked")) {
-          // Prevent the default form submission
-          event.preventDefault();
-          // Display SweetAlert warning
-          swal('Warning', 'You have not checked the box to agree to Terms and Conditions!', 'warning');
-      }
-      else {
-          var redirectUrl = "/Contribution";
-          window.location.href = redirectUrl;
-      }
-  });
+$(document).ready(function () {
+    $("#homeworkForm").submit(function (event) {
+        // Check if the checkbox is checked
+        if (!$("#agree").prop("checked")) {
+            // Prevent the default form submission
+            event.preventDefault();
+            // Display SweetAlert warning
+            swal('Warning', 'You have not checked the box to agree to Terms and Conditions!', 'warning');
+        }
+        else {
+            var redirectUrl = "/Contribution";
+            window.location.href = redirectUrl;
+        }
+    });
 });
-
 
